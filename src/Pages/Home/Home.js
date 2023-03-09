36d5068975412fdd116037746bd56ca30cc663e4
Marfater Rahman Jabed <marfaterrahman@gmail.com>
2023-03-09 18:50:57 +0600

    context

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Button from '../../Components/Button/Button';
+import { AuthContext } from '../../Components/Contexts/Context';
 import Spinner from '../../Components/Spinner/Spinner';
 
 const Home = () => {
+    const { user } = useContext(AuthContext)
     return (
         <>
             <Spinner></Spinner>
+            <p>{user}</p>
             <Button classes={`w-56 h-50 boreder rounded`}>
                 Hello
             </Button>
